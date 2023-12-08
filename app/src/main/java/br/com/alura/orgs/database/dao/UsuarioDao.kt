f7960977ffef2c68ef9434afdf16984f9420c8ad
djefani <djefani.eveeleen@gmail.com>
2023-12-08 12:59:21 -0300

    user

--- app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/UsuarioDao.kt
@@ -2,10 +2,16 @@ package br.com.alura.orgs.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import br.com.alura.orgs.model.Usuario
 
 @Dao
 interface UsuarioDao {
     @Insert
     suspend fun salva(usuario: Usuario)
+
+    @Query("""SELECT * FROM Usuario 
+        WHERE id = :usuarioId 
+        AND senha = :senha""")
+    fun autentica(usuarioId: String, senha: String) : Usuario?
 }
